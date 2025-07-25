e8bb5e91f1aad21948cc0fe17b34075a6d42643e
Mike Chu <michael.chu@optimizely.com>
2025-07-25 16:49:50 -0400

    refactor: OptimizelyApiClient to use loadEnvironment for environment variable loading

--- src/modules/optimizely-client.ts
+++ src/modules/optimizely-client.ts
@@ -1,4 +1,4 @@
-import { loadEnvironmentVariables } from "../config/environment.ts";
+import { loadEnvironment } from "../config/environment.ts";
 import * as logger from "../utils/logger.ts";
 import * as retry from "../utils/retry.ts";
 import * as validation from "../utils/validation.ts";
@@ -47,14 +47,24 @@ export class OptimizelyApiClient {
 
   /**
    * Creates a new OptimizelyApiClient instance.
+   * @param token API token for authentication
    * @param options Client configuration options
    */
-  constructor(options: OptimizelyApiClientOptions = {}) {
-    const env = loadEnvironmentVariables();
-    this.token = env.OPTIMIZELY_API_TOKEN;
+  constructor(token: string, options: OptimizelyApiClientOptions = {}) {
+    this.token = token;
     this.baseUrl = options.baseUrl ?? "https://api.optimizely.com/v2";
     this.maxRps = options.maxRps ?? 5;
     this.maxRetries = options.maxRetries ?? 3;
+  }
+
+  /**
+   * Factory method to create OptimizelyApiClient with environment configuration.
+   * @param options Client configuration options
+   * @returns Promise resolving to OptimizelyApiClient instance
+   */
+  static async create(options: OptimizelyApiClientOptions = {}): Promise<OptimizelyApiClient> {
+    const env = await loadEnvironment();
+    return new OptimizelyApiClient(env.OPTIMIZELY_API_TOKEN, options);
   }
 
   /**
@@ -99,7 +109,7 @@ export class OptimizelyApiClient {
    * @returns Result object with array of OptimizelyFlag or error
    */
   async getAllFeatureFlags(): Promise<Result<OptimizelyFlag[], Error>> {
-    const env = loadEnvironmentVariables();
+    const env = await loadEnvironment();
     const projectId = env.OPTIMIZELY_PROJECT_ID;
     const path = `/flags/v1/projects/${encodeURIComponent(projectId)}/flags`;
     const result = await this.request<{ items: OptimizelyFlag[] }>(path);
